6b3d7ab98ebd3166321de185b5610d4733d73aaa
Ixtiyor0919 <Komiljonov2002@outlook.com>
2023-12-25 11:56:30 +0500

    css ornatildi

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,17 +2,29 @@ import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/pages//*.{js,ts,jsx,tsx,mdx}',
+    './src/components//*.{js,ts,jsx,tsx,mdx}',
+    './src/app//*.{js,ts,jsx,tsx,mdx}',
+    './src/app//*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors:{
+      "green":'#35CC61',
+      'grey':'#838383',
+      'gray':"#A0A0A0",
+      'lighter':'#EBEBEB',
+      'dark':'#000'
+    },
+    screens:{
+      '2xl': {'max': '1750px'},
+      'lxl':{'max':'1600px'},
+      'xl': {'max': '1380px'},
+      'lg': {'max': '1170px'},
+      'md': {'max': '1000px'},
+      'smd': {'max': '770px'},
+      'sm': {'max': '456px'}
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
     },
   },
   plugins: [],
