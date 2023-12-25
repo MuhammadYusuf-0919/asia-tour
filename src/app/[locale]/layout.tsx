6f083b1bedf39591b01ea93e2070fd0f81c34d99
Zoirjon <zoirjonabduvohidov0@gmail.com>
2023-12-25 11:53:38 +0500

    tailwind edited

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,10 +1,20 @@
+import Header from '@/components/Header';
 import '@/styles/globals.css'
-import { dir } from 'i18next';
 import { ReactNode } from "react";
 export default function LocaleLayout({ children, params: { locale } }: { children: ReactNode, params: any }) {
   return (
-    <html lang={locale} dir={dir(locale)}>
-      <body>{children}</body>
+    <html lang={locale}>
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
+        />
+      </head>
+      <body>
+        <Header />
+        {children}
+        {/* Footer */}
+      </body>
     </html>
   );
 }
